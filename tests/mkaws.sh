f2ddde5670f3d94ffb5667e1ac08b4724526f0db
zztampede <zetz.phantazm@gmail.com>
2021-03-21 10:05:57 +0200

    n1

---
+++ tests/mkaws.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+TOKEN=$(php ./tests/travis_create_api_token.php)
+
+cat <<-EOF >> $MANTIS_BOOTSTRAP
+	<?php
+		\$GLOBALS['MANTIS_TESTSUITE_SOAP_ENABLED'] = true;
+		\$GLOBALS['MANTIS_TESTSUITE_SOAP_HOST'] = 'http://$HOSTNAME:$PORT/api/soap/mantisconnect.php?wsdl';
+		\$GLOBALS['MANTIS_TESTSUITE_REST_ENABLED'] = true;
+		\$GLOBALS['MANTIS_TESTSUITE_REST_HOST'] = 'http://$HOSTNAME:$PORT/api/rest/';
+		\$GLOBALS['MANTIS_TESTSUITE_API_TOKEN'] = '$TOKEN';
+	EOF
+
